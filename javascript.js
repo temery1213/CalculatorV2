158613b993a8263079e10219116b82080928e0db
temery1213 <temery1213@gmail.com>
2025-05-19 13:08:49 -0500

    Added function to append numbers on click Changed some classing/ID's in html Added style

--- javascript.js
+++ javascript.js
@@ -1,3 +1,27 @@
+const userInput = document.getElementById('userInput');
+
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+
+let currentInput = '';
+let numbers = [];
+let currentOperator = null;
+
+
+
+
 let addition = (a, b, ...rest) => [a, b, ...rest].reduce((sum, num) => sum + num, 0);
 let subtraction = (a, b, ...rest) => [a, b, ...rest].reduce((result, num) => result - num);
 let multiplication = (a, b, ...rest) => [a, b, ...rest].reduce((prod, num) => prod * num);
@@ -23,3 +47,22 @@ function operation(operator) {
     }
 }
 
+
+    function appendNumberToDisplay(number) {
+        currentInput += number;
+        userInput.textContent += number;
+    };
+
+
+    const numberButtons = document.querySelectorAll(".numberButton");
+
+    numberButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            appendNumberToDisplay(button.textContent);
+        });
+    });
+
+
+
+
+  
